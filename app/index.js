be28073d4f7278208eb38a623bc74b79c8ca3ad7
Vladislav Nazarchuk <Vladislav.Nazarchuk@KYIVSTAR.UA>
2017-12-11 22:01:01 +0200

    add multi paths

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
-const convertExcel = require('excel-as-json').processFile;
-const fs = require('fs');
+const convertCities = require('./module/convertCities');
 
-const SRC = './app/media/all_shops_uk.xlsx';
-const DST = './dist/shops.json';
+
+const paths = [ './app/media/all_shops_uk', './app/media/all_shops_ru' ];
+const dstPath = './dist/';
 
 const OPTIONS_ROW = {
   sheet:'1',
@@ -10,75 +10,4 @@ const OPTIONS_ROW = {
   omitEmtpyFields: true,
 };
 
-/* Need structure JSON files
-  [
-    {
-      "town": "Бердичев",
-      "servicesCenters": ["ул. Европейская, 10", "ул. Житомирская 17"]
-    },
-  ]
- */
-convertExcel(SRC, DST, OPTIONS_ROW, (err, data) => {
-  if (err) console.log(`JSON conversion failure: ${err}`);
-  
-  const objectCities = data.reduce(reducerCO, {});
-
-  const finishStructure = makeFinishStructure(objectCities);
-  
-  const resultJSON = JSON.stringify(finishStructure);
-
-  fs.writeFile('./dist/1.json', resultJSON, (err) => {
-    if (err) throw err;
-    console.log('It\'s saved!');
-  });
-});
-
-/* Функция создает временную JSON структуру ключ которого город
-  {
-    "Lviv": {
-      "town": "Lviv",
-      "serviceCenters": [ "Победы 18", "Стратегическое шосе 5"]
-    }
-  }
- */
-function reducerCO(previous, current) {
-  const newTown = current.town;
-  const newAddress = current.address;
-  const arrAdresses = previous[newTown];
-
-  if ((newTown === undefined || newTown === '')) return previous;
-
-  if (arrAdresses === undefined) {
-    previous[newTown] = {
-      "town": "",
-      "serviceCenters": [],
-    };
-
-    previous[newTown].town = newTown;
-    previous[newTown].serviceCenters = [newAddress];
-
-    return previous;
-  };
-
-  arrAdresses.serviceCenters.push(newAddress);
-
-  return previous;
-};
-
-/* Функция содает массив обектов с городами
-  [
-    {
-      "town": "Львов",
-      "servicesCenters": ["ул. Европейская, 10", "ул. Житомирская 17"],
-    }
-  ]
- */
-function makeFinishStructure(objCities) {
-  const arr = [];
-  
-  for (city in objCities) {
-    arr.push(objCities[city]);
-  };
-
-  return arr;
-};
+convertCities(paths, dstPath, OPTIONS_ROW);
---
+++ app/module/convertCities.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const path = require('path');
+const convertExcel = require('excel-as-json').processFile;
+
+
+/* Need structure JSON files
+  [
+    {
+      "town": "Бердичев",
+      "servicesCenters": ["ул. Европейская, 10", "ул. Житомирская 17"]
+    },
+  ]
+ */
+module.exports = function convertCities (paths, dst, options) {  
+  if (Array.isArray(paths)) {
+    paths.forEach(runConvert);
+
+    return;
+  };
+  
+  runConvert(paths);
+
+  function runConvert (paths) {
+    console.log('function');
+    const SRC = paths + '.xlsx';
+    const DST = dst + path.basename(paths) + '.json';
+    
+    convertExcel(SRC, null, options, (err, data) => {
+      if (err)
+        console.log(`JSON conversion failure: ${err}`);
+
+      const objectCities = data.reduce(reducerCO, {});
+
+      const finishStructure = makeFinishStructure(objectCities);
+
+      const resultJSON = JSON.stringify(finishStructure);
+      
+      fs.writeFile(DST, resultJSON, (err) => {
+        if (err)
+          throw err;
+
+        console.log('It\'s saved!', DST);
+      });
+    });
+  };
+};
+
+/* Функция создает временную JSON структуру ключ которого город
+  {
+    "Lviv": {
+      "town": "Lviv",
+      "serviceCenters": [ "Победы 18", "Стратегическое шосе 5"]
+    }
+  }
+ */
+function reducerCO(previous, current) {
+  const newTown = current.town;
+  const newAddress = current.address;
+  const arrAdresses = previous[newTown];
+
+  if ((newTown === undefined || newTown === '')) return previous;
+
+  if (arrAdresses === undefined) {
+    previous[newTown] = {
+      "town": "",
+      "serviceCenters": [],
+    };
+
+    previous[newTown].town = newTown;
+    previous[newTown].serviceCenters = [newAddress];
+
+    return previous;
+  };
+
+  arrAdresses.serviceCenters.push(newAddress);
+
+  return previous;
+};
+
+/* Функция содает массив обектов с городами
+  [
+    {
+      "town": "Львов",
+      "servicesCenters": ["ул. Европейская, 10", "ул. Житомирская 17"],
+    }
+  ]
+ */
+function makeFinishStructure(objCities) {
+  const arr = [];
+  
+  for (city in objCities) {
+    arr.push(objCities[city]);
+  };
+
+  return arr;
+};
