23a1c247262a0dfe20ace6ee44b7a49616eb7934
Vladislav Nazarchuk <Vladislav.Nazarchuk@KYIVSTAR.UA>
2018-03-01 13:41:32 +0200

    configure constants

--- config/env.mjs
+++ config/env.mjs
@@ -1,25 +1,25 @@
-export default function (NODE_ENV) {
-  if (process.env.NODE_ENV !== 'production') return development();
-  return production();
-}
-
-function production () {
-  return {
-    PORT: process.env.PORT,
-    ...common()
-  }
-};
-
-function development () {
-  return {
-    PORT: process.env.PORT_DEV,
-    ...common()
-  }
-};
-
-function common () {
+function common() {
   return {
     DIST_DIR: process.env.DIST_DIR,
     FILE_LIMIT: process.env.FILE_LIMIT,
-  }
+  };
+}
+
+function production() {
+  return {
+    PORT: process.env.PORT,
+    ...common(),
+  };
+}
+
+function development() {
+  return {
+    PORT: process.env.PORT_DEV,
+    ...common(),
+  };
+}
+
+export default function() {
+  if (process.env.NODE_ENV !== 'production') return development();
+  return production();
 }
